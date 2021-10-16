954d8c900ce472a23b69ecd591180a77422d0e6a
ZbyszekAdamek <zbigniew.t.adamek@gmail.com>
2021-10-16 16:45:46 +0200

    Ponowne rozpoczęcie ze Spring Security

--- src/main/java/pl/coderslab/DrinkApp/SecurityConfig.java
+++ src/main/java/pl/coderslab/DrinkApp/SecurityConfig.java
@@ -1,11 +1,10 @@
-/*package pl.coderslab.DrinkApp;
+package pl.coderslab.DrinkApp;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation
-        .web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -14,8 +13,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/home").permitAll()
-                .antMatchers("/admin/**").authenticated()
+                .antMatchers("/dashboard").authenticated()
                 .and().formLogin()
                 .loginPage("/login");
     }
-}*//**/
+}
