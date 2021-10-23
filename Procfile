11973a2804c100a60c1d64062f8efe7971d5ca23
ZbyszekAdamek <zbigniew.t.adamek@gmail.com>
2021-10-23 15:01:59 +0200

    Drobne poprawki w kodzie

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: java -jar target/DrinkApp-0.0.1.jar
--- src/main/java/pl/coderslab/DrinkApp/SecurityConfig.java
+++ src/main/java/pl/coderslab/DrinkApp/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http.csrf().disable().authorizeRequests()
                 .antMatchers("/home","/findRecipe").permitAll()
                 .antMatchers("/dashboard").authenticated()
                 .and().formLogin().defaultSuccessUrl("/dashboard",true)
